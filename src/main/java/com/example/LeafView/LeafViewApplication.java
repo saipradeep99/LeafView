00e662b8d2f8d1bb2482ed387842f95d47891fe4
Pradeep <saipradeeep99@gmail.com>
2025-04-06 22:42:16 +0530

    Initial Commit

---
+++ src/main/java/com/example/LeafView/LeafViewApplication.java
@@ -0,0 +1,13 @@
+package com.example.LeafView;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeafViewApplication.class, args);
+	}
+
+}
